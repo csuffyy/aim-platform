4cde34fc39a82e5265bdea1e3be4891d24ce3c06
Daniel Snider <danielsnider12@gmail.com>
2019-03-11 20:41:14 -0700

    jen

--- image-archive/environments/development/env.groovy
+++ image-archive/environments/development/env.groovy
@@ -3,4 +3,4 @@ env.ELASTIC_IP='172.20.4.85'
 env.ELASTIC_PORT=9200
 env.ELASTIC_INDEX='image'
 env.ELASTIC_DOC_TYPE='image'
-env.ES_JAVA_OPTS="\\\'-Xms16g -Xmx16g\\\'"
+env.ES_JAVA_OPTS='\\\\\\"-Xms16g -Xmx16g\\\\\\"'
