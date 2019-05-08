af008b3aac5caa9cf1434d25c2b82e14f315e357
Daniel Snider <danielsnider12@gmail.com>
2019-05-08 11:34:30 -0400

    install.sh

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -74,11 +74,37 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install -y docker-ce
 
-# install Tmuxinator (=>v0.11)
+# Install Tmuxinator (=>v0.11)
 git clone https://github.com/tmuxinator/tmuxinator
 cd tmuxinator
 gem install tmuxinator
 tmuxinator -v
+
+# Install deid dependencies (python and etc.)
+sudo apt-get install -y python3-tk # for matplotlib
+sudo apt-get install -y python3.7-dev libpython3.7-dev python3-pip
+# sudo pip3 install virtualenv
+# virtualenv -p python3.7 venv
+# cd venv/
+# . bin/activate
+pip3.7 install numpy IPython scikit-image matplotlib pandas Pillow click pydicom deid pytesseract opencv-python python-Levenshtein fuzzywuzzy fuzzywuzzy[speedup] elasticsearch dateparser
+
+# Install GDCM
+sudo apt-get install python-gdcm # for pydicom
+git clone --branch master https://github.com/HealthplusAI/python3-gdcm.git && cd python3-gdcm && dpkg -i build_1-1_amd64.deb && apt-get install -f
+sudo cp /usr/local/lib/gdcm.py /usr/local/lib/python3.7/dist-packages/
+sudo cp /usr/local/lib/gdcmswig.py /usr/local/lib/python3.7/dist-packages/
+sudo cp /usr/local/lib/_gdcmswig.so /usr/local/lib/python3.7/dist-packages/
+sudo cp /usr/local/lib/libgdcm* /usr/local/lib/python3.7/dist-packages/
+
+# cp /usr/local/lib/gdcm.py ./lib/python3.7/site-packages/
+# cp /usr/local/lib/gdcmswig.py ./lib/python3.7/site-packages/
+# cp /usr/local/lib/_gdcmswig.so ./lib/python3.7/site-packages/
+# cp /usr/local/lib/libgdcm* ./lib/python3.7/site-packages/
+
+# Developer helper tools:
+# sudo apt-get install vtk-dicom-tools
+# dicomdump file1.dcm 
 
 ###############################
 # Production Server Dependencies
