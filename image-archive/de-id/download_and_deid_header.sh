f5aaa4a8bd9c2a6b93d15581a39eb3de8da713af
Daniel Snider <danielsnider12@gmail.com>
2019-05-21 12:14:12 -0400

    wip

--- image-archive/de-id/download_and_deid_header.sh
+++ image-archive/de-id/download_and_deid_header.sh
@@ -8,8 +8,8 @@ git pull
 # Create Elastic index to store de-id mappings
 source ./environments/production/env.sh
 export ELASTIC_IP='192.168.100.61' # special elastic location via tunnel when in HPF
-export LINKING_ELASTIC_INDEX=linking-anna-alex-abhi
-export LINKING_ELASTIC_DOC_TYPE=linking-anna-alex-abhi
+export LINKING_ELASTIC_INDEX=linking-anna-alex-abhi-2
+export LINKING_ELASTIC_DOC_TYPE=linking-anna-alex-abhi-2
 ./elastic-search/init_elastic.sh
 
 # Make output path
@@ -46,8 +46,8 @@ set -x
 
 source ~/aim-platform/image-archive/environments/production/env.sh
 export ELASTIC_IP='192.168.100.61' # special elastic location via tunnel when in HPF
-export LINKING_ELASTIC_INDEX=linking-anna-alex-abhi
-export LINKING_ELASTIC_DOC_TYPE=linking-anna-alex-abhi
+export LINKING_ELASTIC_INDEX=linking-anna-alex-abhi-2
+export LINKING_ELASTIC_DOC_TYPE=linking-anna-alex-abhi-2
 
 module load python/3.7.1_GDCM
 
