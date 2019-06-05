2f2847108ef84eb4eb7d7ae4b98eb95f8fc48ec2
Daniel Snider <danielsnider12@gmail.com>
2019-06-05 16:11:25 -0400

    tmux improve

--- image-archive/elastic-search/start_elastic.sh
+++ image-archive/elastic-search/start_elastic.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -d --name elasticsearch -p 9200:9200 -p 9300:9300 -e ES_JAVA_OPTS="-Xms16g -Xmx16g" -v `pwd`/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml -v /home/ubuntu/esdata:/usr/share/elasticsearch/data docker.elastic.co/elasticsearch/elasticsearch:6.7.1
+sudo docker run -d --name elasticsearch -p 9200:9200 -p 9300:9300 -e ES_JAVA_OPTS="-Xms32g -Xmx32g" -v `pwd`/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml -v /home/ubuntu/esdata:/usr/share/elasticsearch/data docker.elastic.co/elasticsearch/elasticsearch:6.7.1
--- image-archive/environments/production/start_staticwebserver.sh
+++ image-archive/environments/production/start_staticwebserver.sh
@@ -4,3 +4,9 @@ cd /hpf/largeprojects/diagimage_common/
 export TOKEN=771100
 #nohup python /home/dsnider/hpf_static_server.py & # TODO: log rotate needed for nohup.out here
 nohup sh -c 'while :; do python /home/dsnider/hpf_static_server.py; done > ~/hpf_static_server.log 2>&1' >/dev/null  &
+
+
+
+# ln -s /mnt/hpf/src/ ./src
+# ln -s /mnt/hpf/shared/ ./shared
+
