26b42d2913e9649dfd4fc9f428e284d8ce1deb7f
Daniel Snider <danielsnider12@gmail.com>
2019-06-14 18:14:38 -0700

    updtae

--- image-archive/elastic-search/elastic_associate_reports_with_images.py
+++ image-archive/elastic-search/elastic_associate_reports_with_images.py
@@ -1,8 +1,12 @@
-# Usage:
-  #qlogin #Specific to HPF
-  #source ./image-archive/environments/local/env.sh
-  #export ELASTIC_IP='192.168.100.61' #Specific to HPF
-  #python3 ./image-archive/elastic-search/elastic_associate_reports_with_images.py  --input_range 0-4
+## Usage:
+# source ../environments/local/env.sh
+# python3 elastic_associate_reports_with_images.py  --input_range 0-4
+#
+# qlogin #Specific to HPF
+# source ./image-archive/environments/local/env.sh
+# export ELASTIC_IP='192.168.100.61' #Specific to HPF
+# python3 ./image-archive/elastic-search/elastic_associate_reports_with_images.py  --input_range 0-4
+
 
 import re
 import os
@@ -10,26 +14,36 @@ import sys
 import json
 import time
 import pydicom
+import logging
 import argparse
+import traceback
 
 from elasticsearch import Elasticsearch
 from IPython import embed
 
+logging.basicConfig(format='%(asctime)s.%(msecs)d[%(levelname)s] %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO)
+                    # level=logging.DEBUG)
+log = logging.getLogger('main')
+
 
 ELASTIC_IP = os.environ['ELASTIC_IP']
 ELASTIC_PORT = os.environ['ELASTIC_PORT']
-DICOM_BASE_PATH = '../reactive-search/'
+DICOM_BASE_PATH = os.environ.get('DICOM_BASE_PATH','../reactive-search/')
 
 def update_dicoms(report, dicom_filepaths):
   """Add report info to metadata in dicom on disk"""
   # TODO: check for unused group
   # TODO: check for unused key
+  if not report:
+    return
 
   # Loop over images. There may be multiple images related to the report
   for filepath in dicom_filepaths:
     filepath = os.path.join(DICOM_BASE_PATH, filepath)
-    ds = pydicom.dcmread(filepath)
-    dicom_datatype = 'LT' # Dicom datatype Value Representation
+    dicom = pydicom.dcmread(filepath)
+    dicom_datatype = 'LT' # Dicom datatype Value Representation "LT" is for Long Text with 10240 chars maximum
     loc_group = 0x0019
     loc_element = 0x0030
 
@@ -39,18 +53,17 @@ def update_dicoms(report, dicom_filepaths):
       key = re.sub(r"[^\w\s]", '', key)
       key = re.sub(r"\s+", '', key)
       loc = (loc_group, loc_element)
+      if loc in dicom:
+        # log.warning('This DICM already has a value at position %s=%s' % (loc, str(dicom[loc])[0:100]))
+        pass
       value = "Report %s: %s" % (key, str(value)) # Append key name to start of value because dicom uses hexadecimal instead of key names
-      # ds[loc] = pydicom.DataElement(loc, dicom_datatype, value)
-      print('Inserting Value: %s' % value)
-      ds[loc] = pydicom.DataElement(loc, dicom_datatype, value)
-      # embed()
-      # ds[0x0019, 0x0030] = pydicom.DataElement(0x00190030, 'LO', 'Assumed TransferSyntaxUID')
+      log.debug('Inserting Value: %s' % value)
+      dicom[loc] = pydicom.DataElement(loc, dicom_datatype, value)
       loc_element = loc_element + 1 # increment to use higher index on next loop
-      # break
 
     # Save updated dicom
-    ds.save_as(filepath)
-    print('Saved DICOM: %s' % filepath)
+    dicom.save_as(filepath)
+    log.info('Saved DICOM: %s' % filepath)
 
 
 def make_update_script_from_report(report):
@@ -65,15 +78,6 @@ def make_update_script_from_report(report):
   update_script = ""
 
   for key, value in report.items():
-    # NOTE: Commented section out because I am now prefixing field names with rr, so overlaps of field names are very unlikely now.
-    # Skip blacklisted keys
-    # # Skip anything with id in it. Study ID, PatientId, etc. These are too important to potentially overwrite
-    # if key.lower().find('id') >= 0:
-    #   continue
-    # # Replace filepath with report_filepath so that it is more specific
-    # if key == 'filepath':
-    #   key = 'report_filepath'
-
     blacklist = ['original_title', 'searchallblank']
     if key in blacklist:
       continue
@@ -88,7 +92,6 @@ def make_update_script_from_report(report):
     # Append the adding of a new new key value to update script
     update_script = update_script + "ctx._source.%s = '%s'; " % (key, value)
 
-  # print(update_script)
   return update_script
 
 # Process hits here
@@ -96,16 +99,23 @@ def process_hits(hits):
   for item in hits:
     report = item['_source']
     AccessionNumber = report['Accession Number']
-    print("Processing AccessionNumber: %s" % AccessionNumber, file=sys.stderr, flush=True)
+    log.info("\nProcessing AccessionNumber: %s" % AccessionNumber)
 
     update = make_update_script_from_report(report)
 
+    # Insert report information into image in ElasticSearch documents that match AccessionNumber.
     # https://elasticsearch-py.readthedocs.io/en/master/api.html#elasticsearch.Elasticsearch.update_by_query
-    es.update_by_query(index=to_index_name, doc_type=to_index_name, body={
+    result = es.update_by_query(index=to_index_name, doc_type=to_index_name, body={
       'query': {'term': {'AccessionNumber.keyword': AccessionNumber}},
       'script': {"inline": update}}
     )
+    if result['total'] == 0:
+      log.info('Nothing found for AccessionNumber')
+      continue
 
+    log.info('Inserting report data into ElasticSearch %s documents. Full response: %s' % (result['total'], result))
+
+    # Get filepaths of the images that we just updated.
     # NOTE: this code block can be removed after this issue is resolved
     # https://github.com/elastic/elasticsearch-py/issues/783
     results = es.search(index=to_index_name, doc_type=to_index_name, body={
@@ -114,8 +124,6 @@ def process_hits(hits):
     )
     dicom_filepaths = [hit['_source']['dicom_filepath'] for hit in results['hits']['hits']]
 
-    if dicom_filepaths == []:
-      print('Nothing found for AccessionNumber')
 
     # Add report info to metadata in dicom on disk
     update_dicoms(report, dicom_filepaths)
@@ -149,7 +157,7 @@ if __name__ == '__main__':
 
   # Check index exists
   if not es.indices.exists(index=from_index_name):
-    print("Index " + from_index_name + " not exists")
+    log.info("Index " + from_index_name + " not exists")
     exit()
 
   # Init scroll by search
@@ -164,9 +172,14 @@ if __name__ == '__main__':
   t0 = time.time()
   count = 0
 
-  process_hits(data['hits']['hits'])
+  try:
+    process_hits(data['hits']['hits'])
+  except:
+    log.error(traceback.format_exc())
+    log.error('Skipping this image because of unknown error ^')
 
   elapsed_time = time.time() - t0
   dl_rate = count / elapsed_time
-  print('\n{} images were updated in Elastic Search '.format(count) + 'in {:.2f} seconds.'.format(elapsed_time), file=sys.stderr)
-  print('Update rate (documents/s): {:.2f}'.format(dl_rate), file=sys.stderr)
+  log.info('\n{} images were updated in Elastic Search '.format(count) + 'in {:.2f} seconds.'.format(elapsed_time))
+  log.info('Update rate (documents/s): {:.2f}'.format(dl_rate))
+  
