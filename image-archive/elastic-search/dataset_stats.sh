2f354b6bc080ce045cb7c6aed8668086f334d79a
Daniel Snider <danielsnider12@gmail.com>
2019-06-17 01:22:11 -0400

    deid update

--- image-archive/elastic-search/dataset_stats.sh
+++ image-archive/elastic-search/dataset_stats.sh
@@ -64,4 +64,16 @@ Unique_SeriesNumber=$(curl -s -H 'Content-Type: application/json' -XGET 'http://
 
 echo "Unique SeriesNumber: $(echo $Unique_SeriesNumber | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta')"
 
+# Count Pixels
+curl -s -X POST "localhost:9200/image/_search" -H 'Content-Type: application/json' --data-binary @count_pixel_agg.json | jq .aggregations.type_count.value
 
+# Count all values in all fields
+for FIELD_NAME in $(curl -s -XGET localhost:9200/image/_mapping | jq .image.mappings.image.properties | jq 'keys' | jq .[]); do FIELD_NAME=$(echo "$FIELD_NAME" | tr -d '"') ;  NUM_NOT_NULL=$(curl -s -X POST "localhost:9200/image/_search?size=0" -H 'Content-Type: application/json' -d'
+{
+    "aggs" : {
+        "types_count" : { "value_count" : { "field" : "'$FIELD_NAME'.keyword" } }
+    }
+}
+' | jq .aggregations.types_count.value);   echo "$NUM_NOT_NULL"; done > out 
+
+paste -sd+ out | bc
